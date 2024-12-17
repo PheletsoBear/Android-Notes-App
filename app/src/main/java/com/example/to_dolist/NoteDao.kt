fbe6236debc64abad2c1d528cc954f20aba84d7b
Pheletso Mashalan <mashalaneevanspheletso@gmail.com>
2024-12-17 20:24:11 +0200

    created ROOM

--- app/src/main/java/com/example/to_dolist/NoteDao.kt
+++ app/src/main/java/com/example/to_dolist/NoteDao.kt
@@ -1,4 +1,25 @@
 package com.example.to_dolist
 
+import android.provider.ContactsContract.CommonDataKinds.Note
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
 interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(notes: Note)
+
+    @Update
+    suspend fun  update(note: Notes)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes Order BY id DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
 }
