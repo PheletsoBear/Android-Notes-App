e367aa3f4e1455c41e467e019d094267ed5c0852
Pheletso Mashalan <mashalaneevanspheletso@gmail.com>
2025-03-10 16:59:48 +0200

    I am back

--- app/src/main/java/com/example/to_dolist/NoteAdapter.kt
+++ app/src/main/java/com/example/to_dolist/NoteAdapter.kt
@@ -1,43 +1,53 @@
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.to_dolist.Notes
 import com.example.to_dolist.databinding.NotesListBinding
 import com.google.android.material.snackbar.Snackbar
 
-class NotesAdapter(private val context: Context, private val notes: MutableList<Notes>) :
+class NotesAdapter(private var notes: MutableList<Notes>,private val context: Context) :
     RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
 
-    inner class NoteViewHolder(val binding: NotesListBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class NoteViewHolder(val binding: NotesListBinding) : RecyclerView.ViewHolder(binding.root){
+
+    fun bind(note: Notes){
+       binding.apply {
+        noteTitle.text = note.title
+         contentDisplay.text = note.content
+           binding.noteDate.text = note.date
+           root.setOnClickListener {
+
+               val removedNote = notes[adapterPosition]
+               val removedPosition = adapterPosition
+
+               notes.removeAt(adapterPosition)
+               notifyItemRemoved(adapterPosition)
+
+               Snackbar.make(binding.root, "Note deleted", Snackbar.LENGTH_LONG)
+                   .setAction("Undo") {
+                       notes.add(removedPosition, removedNote)
+                       notifyItemInserted(removedPosition)
+                   }.show()
+           }
+       }
+
+        }
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
     ): NoteViewHolder {
-        val binding = NotesListBinding.inflate(LayoutInflater.from(context), parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = NotesListBinding.inflate(inflater, parent, false)
         return NoteViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: NotesAdapter.NoteViewHolder, position: Int) {
-        val note = notes[position]
-        holder.binding.noteTitle.text = note.title
-        holder.binding.contentDisplay.text = note.content
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(notes[position])
 
-        holder.binding.root.setOnClickListener{
-            val removedNote = notes[position]
-            val removedPosition = position
-
-            notes.removeAt(position)
-            notifyItemRemoved(position)
-
-            Snackbar.make(holder.binding.root, "Note deleted", Snackbar.LENGTH_LONG)
-                .setAction("Undo") {
-                    notes.add(removedPosition, removedNote)
-                    notifyItemInserted(removedPosition)
-                }.show()
-           true
-        }
     }
 
     override fun getItemCount(): Int {
