34c3467d4b05e361f5349c1480d132578d8368d4
Pheletso Mashalan <mashalaneevanspheletso@gmail.com>
2025-04-12 03:31:18 +0200

    moved to another databse version

--- app/src/main/java/com/example/to_dolist/data/local/NoteDatabase.kt
+++ app/src/main/java/com/example/to_dolist/data/local/NoteDatabase.kt
@@ -5,8 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.to_dolist.data.local.NoteDao
 import com.example.to_dolist.data.local.Notes
 
-@Database(entities = [Notes::class], version = 1, exportSchema = false)
-
+@Database(entities = [Notes::class], version = 2, exportSchema = false)
 abstract class NoteDatabase: RoomDatabase() {
 
     abstract fun noteDao(): NoteDao
